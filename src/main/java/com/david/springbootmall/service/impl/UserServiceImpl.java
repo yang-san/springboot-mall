2b282d1f944504763f4505ffb68f88a5b24ac931
david <yangdavid0614@gmail.com>
2025-05-27 16:13:35 +0800

    帳號功能 - 使用 MD5 加密密碼

--- src/main/java/com/david/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/david/springbootmall/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -23,13 +24,18 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        // 檢查註冊的 email
         User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
         if (user != null) {
             logger.warn("email {} 已經被註冊 ", userRegisterRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
-        Integer userId = userDao.createUser(userRegisterRequest);
-        return userId;
+        // 使用 MD5 生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
+        // 創建帳號
+        return userDao.createUser(userRegisterRequest);
     }
 
     @Override
@@ -46,7 +52,10 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email 尚未註冊");
         }
 
-        if (user.getPassword().equals(userLoginRequest.getPassword())) {
+        // 使用 MD5 生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        if (user.getPassword().equals(hashedPassword)) {
             return user;
         } else {
             logger.warn("email {} 密碼錯誤", userLoginRequest.getEmail());
